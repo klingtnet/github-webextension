e5191698f68322a3bb0400790d3a96d1492340ef
Andreas Linz <klingt.net@gmail.com>
2023-06-21 09:33:45 +0200

    Adjust to new GitHub header
    The box-shadow will get applied to the header element now. As a
    fallback, if the old header style gets served, the repository-container-
    header is used. Also, we now detect if a repository is public or not by
    inspecting the octolytics- dimension-repository_public meta header. Both
    changes should make the extension a bit more robust.

--- webextension.js
+++ webextension.js
@@ -1,6 +1,9 @@
-var header = document.getElementById("repository-container-header");
-var labels = Array.from(header.getElementsByClassName("Label"));
-var has_public_label = labels.filter(label => label.innerText.includes("Public")).length > 0;
-if (has_public_label) {
+var header = document.querySelector('header[class="AppHeader"]');
+if (!header) {
+    header = document.getElementById("repository-container-header");
+}
+
+var public = document.head.querySelector('meta[name="octolytics-dimension-repository_public"]');
+if ('content' in public && public.content) {
     header.style.boxShadow = "0 0 5em darkred";
 }

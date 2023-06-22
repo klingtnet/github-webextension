108bcc73c874bd4d4ad024244c5882e8c9ad1959
Andreas Linz <klingt.net@gmail.com>
2023-06-22 08:00:16 +0200

    Make shadow visible when there's a notification shelf
    If there's a notification shelf, then the shadow was hidden underneath
    it.  We now set the z-index of the AppHeader so that its shadow will
    lay over elements below.  Setting a z-index requires the position of the
    element to be relative (default is static position).

--- webextension.js
+++ webextension.js
@@ -6,4 +6,6 @@ if (!header) {
 var public = document.head.querySelector('meta[name="octolytics-dimension-repository_public"]');
 if ('content' in public && public.content !== "false") {
     header.style.boxShadow = "0 0 5em darkred";
+    header.style.position = "relative";
+    header.style.zIndex = 1000;
 }
